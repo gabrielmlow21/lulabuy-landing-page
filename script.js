e1c06353c86396b1ddd3c33edbe066b2aecb8997
Gabriel Low <lowgabriel@ymail.com>
2021-12-02 15:21:40 +0800

    updated object iteration method

--- script.js
+++ script.js
@@ -3,18 +3,31 @@ fetch('./faq.json')
     .then(res => res.json())
     .then(data => {
         // adding categories
-        for (var i = 0; i < Object.keys(data).length; i++) {
-            var category = Object.keys(data)[i];
+        // for (var i = 0; i < Object.keys(data).length; i++) {
+        //     var category = Object.keys(data)[i];
+            
+        //     var li = document.createElement('li');
+        //     li.appendChild(document.createTextNode(category));
+        //     li.classList.add("categories__item")
+        //     li.setAttribute('data-category', i)
+        //     categoriesContainer.appendChild(li);
+        // }
+
+        Object.entries(data).forEach(([category, content], index) => {
             var li = document.createElement('li');
             li.appendChild(document.createTextNode(category));
             li.classList.add("categories__item")
-            li.setAttribute('data-category', i)
+            li.setAttribute('data-category', index)
             categoriesContainer.appendChild(li);
-        }
+            Object.entries(content).forEach(([question, answer], index) => {
+                
+            });
+        });
     });
 
 // DOMS
 const categoriesContainer = document.querySelector('.categories__container');
+const questionAnswerContainer = document.querySelector('.question-answer__container');
 
 categoriesContainer.addEventListener('click', function(e) {
     // must put here because element gets loaded late
