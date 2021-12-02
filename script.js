78e085b7294d5144e7d099893fc9132b130207fa
Gabriel Low <lowgabriel@ymail.com>
2021-12-02 13:43:24 +0800

    added active category code

--- script.js
+++ script.js
@@ -2,15 +2,29 @@
 fetch('./faq.json')
     .then(res => res.json())
     .then(data => {
-        const categoriesEl = document.querySelector('.categories__items');
         // adding categories
         for (var i = 0; i < Object.keys(data).length; i++) {
             var category = Object.keys(data)[i];
             var li = document.createElement('li');
             li.appendChild(document.createTextNode(category));
             li.classList.add("categories__item")
-            categoriesEl.appendChild(li);
+            li.setAttribute('data-category', i)
+            categoriesContainer.appendChild(li);
         }
     });
 
 // DOMS
+const categoriesContainer = document.querySelector('.categories__container');
+
+categoriesContainer.addEventListener('click', function(e) {
+    // must put here because element gets loaded late
+    const categoriesItems = document.querySelectorAll('.categories__item');
+
+    const clicked = e.target.closest('.categories__item');
+
+    // Guard Clause
+    if (!clicked) return;
+
+    categoriesItems.forEach(c => c.classList.remove('categories__item-active'));
+    clicked.classList.add('categories__item-active');
+})
