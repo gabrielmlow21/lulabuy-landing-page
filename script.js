92d5f1e5ba2cad6c457f8154efc2f2156802d0ce
Gabriel Low <lowgabriel@ymail.com>
2021-12-06 18:18:39 +0800

    added scroll event animation

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ categoriesContainer.addEventListener('click', function(e) {
     document.querySelectorAll('.question-answer__container').forEach(c => c.classList.remove('question-answer__container-active'));
     document.querySelectorAll(`.question-answer__container-${clicked.dataset.category}`).forEach(c => c.classList.add('question-answer__container-active'));
 })
+
+
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function(entries, observer) {
+    const [entry] = entries;
+    console.log(entry);
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver
+(revealSection, {
+    root: null,
+    threshold: 0.15
+});
+
+allSections.forEach(function (section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+})
